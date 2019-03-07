faf640d7ea93e91b73989817fd9bcb244437a60f
jennysujin <48301644+jennysujin@users.noreply.github.com>
2019-03-07 10:38:40 +0900

    C와 유사한 java문법 맞보기
    미나얼미나어리

---
+++ 0308/src/review_C/DoubleTest.java
@@ -0,0 +1,15 @@
+package review_C;
+
+public class DoubleTest {
+	public static void name(String[] args) {
+		double num1, num2;
+		double result;
+		num1 = 2.000001;
+		num2 = 1.000001;
+		result= num1+num2;
+		
+		System.out.println(result);
+	
+}
+
+}
